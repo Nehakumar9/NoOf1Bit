c6e1574abc21014072fc9504bcca2a96c44cce13
Nehakumar9 <nehakumar0901@gmail.com>
2023-07-18 16:22:10 +0530

    no of bit commited

---
+++ src/noof1bit/NoOf1Bit.java
@@ -0,0 +1,25 @@
+package noof1bit;
+
+import java.util.Scanner;
+
+public class NoOf1Bit {
+
+	public static int numberOfBits(int number){
+        int count = 0;
+        while (number > 0){
+            count += ((number & 1) == 0) ? 0 : 1;
+            number = number >> 1 ;
+        }
+        return count;
+    }
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Scanner scan = new Scanner(System.in);
+	int number = scan.nextInt();
+	int result = numberOfBits(number);
+	System.out.println(result);
+
+	}
+
+}
